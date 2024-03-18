cd139c7ad4d3be2d37f4deb24990b7c7c5a4a852
Patrycja Sander <patrycja.sander2@gmail.com>
2024-03-18 13:52:39 +0100

    setup

---
+++ src/main/java/com/project/schedule_official/ScheduleOfficialApplication.java
@@ -0,0 +1,13 @@
+package com.project.schedule_official;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleOfficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleOfficialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/schedule_official/ScheduleOfficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.schedule_official;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleOfficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
